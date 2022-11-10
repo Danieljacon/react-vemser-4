c0d79ac0d45469a91344a36fea408331a9fd9184
Danieljacon <danielcamillo2020@gmail.com>
2022-11-10 01:38:32 -0300

    Finalizada a página

--- src/pages/NewTicket/NewTicket.styled.ts
+++ src/pages/NewTicket/NewTicket.styled.ts
@@ -1,1 +1,50 @@
 import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  padding: 20px;
+
+  > .location {
+    padding: 0 20px;
+    p {
+      color: #8d91ab;
+    }
+  }
+
+  > .my-tickets {
+    margin-top: 20px;
+    > h1 {
+      font-size: 1.5rem;
+      font-weight: 600;
+      color: #2f3367;
+    }
+
+    > .ticket {
+      margin-top: 20px;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      > h3 {
+        font-size: 1.5rem;
+        font-weight: 600;
+        color: #2f3367;
+        margin-top: 60px;
+        text-align: center;
+      }
+
+      > p {
+        font-size: 1rem;
+        color: #868aa5;
+        margin-bottom: 60px;
+        text-align: center;
+      }
+
+      > a button {
+        padding: 10px 20px;
+      }
+    }
+  }
+`;
--- src/pages/NewTicket/index.tsx
+++ src/pages/NewTicket/index.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { Container } from "../../components/common.styled";
 import { Header } from "../../components/Header";
+import { Wrapper } from "./NewTicket.styled";
+import iconFile from "../../assets/icon-file.png";
+import { Button } from "../../components/Button";
 
 export const NewTicket: React.FC = () => {
   return (
     <Container>
       <Header />
+      <Wrapper>
+        <div className="location">
+          <p>
+            My Panel {">"} Ticket {">"} #invoice
+          </p>
+        </div>
+        <div className="my-tickets">
+          <h1>My Tickets</h1>
+
+          <div className="ticket">
+            <img src={iconFile} alt="" />
+            <h3>You do not gave any tickets yet!</h3>
+            <p>Send your first ticket from here.</p>
+            <Button buttonType="primary">ADD NEW TICKET</Button>
+          </div>
+        </div>
+      </Wrapper>
     </Container>
   );
 };
